2be9d5806467a0165984e92432d68b9a1429a220
Stas <zodovich@gmail.com>
2026-06-19 02:47:18 +0300

    Changed version

--- Formula/kubie-go.rb
+++ Formula/kubie-go.rb
@@ -1,17 +1,17 @@
 class KubieGo < Formula
   desc "Kubernetes context and namespace manager written in Go"
   homepage "https://github.com/stanislavtrubachev/kubie-go"
-  version "0.1.0"
+  version "0.1.2"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/stanislavtrubachev/kubie-go/releases/download/v0.1.0/kubie-go_0.1.0_darwin_arm64.tar.gz"
-      sha256 "aaf46df7ae13caae049e346621a6280cabbe55aa84aaafd4539f2efafe4bb9e0"
+      url "https://github.com/stanislavtrubachev/kubie-go/releases/download/v0.1.0/kubie-go_0.1.2_darwin_arm64.tar.gz"
+      sha256 "39d133f57dbc8a0dd46f806648d734590867d3a6ef3c503edf30ced1645df41d"
     end
     on_intel do
-      url "https://github.com/stanislavtrubachev/kubie-go/releases/download/v0.1.0/kubie-go_0.1.0_darwin_amd64.tar.gz"
-      sha256 "0322a98116ac68aec82b4949e33c40c6d2959d08efbc1bb46ee259aec0f97bdf"
+      url "https://github.com/stanislavtrubachev/kubie-go/releases/download/v0.1.0/kubie-go_0.1.2_darwin_amd64.tar.gz"
+      sha256 "c26160a1edc174ab42b94d41e5e25aa4355269715b63e784928614be223b62be"
     end
   end
 
